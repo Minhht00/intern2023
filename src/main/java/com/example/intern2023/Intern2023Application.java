dcebbbbadb2731ba415a7a6b17d0fc192d5b9096
Minht00 <luongtuanminh02161@gmail.com>
2023-08-18 15:43:57 +0700

    create

---
+++ src/main/java/com/example/intern2023/Intern2023Application.java
@@ -0,0 +1,13 @@
+package com.example.intern2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Intern2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Intern2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/intern2023/Intern2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intern2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Intern2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
